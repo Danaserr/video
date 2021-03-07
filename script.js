a9e5c7ce0abdf086cde4c063f06fef01510daa76
Danaserr <80220517+Danaserr@users.noreply.github.com>
2021-03-08 01:40:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+(function () {
+	const header = document.querySelector('.header');
+	window.onscroll = () => {
+		if (window.pageYOffset > 50) {
+			header.classList.add('header__active');
+		} else {
+			header.classList.remove('header__active');
+		}
+	};
+}());
+
+(function(){
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menucloseItem = document.querySelector('.header__nav-closee');
+	const menuLinks = document.querySelectorAll('.header__link');
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header__nav_active');
+	});
+	menucloseItem.addEventListener('click', () => {
+		menu.classList.remove('header__nav_active');
+	});
+	if (window.innerWidth <= 770){
+		for (let i = 0; i < menuLinks.length; i += 1){
+			menuLinks[i].addEventListener('click', () => {
+				menu.classList.remove('header__nav_active');
+			});
+		}
+	}
+}());
